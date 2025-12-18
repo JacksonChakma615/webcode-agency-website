65c9eb2efd7ded13649529ca547d29bf711ae9ac
Jackson Chakma <jacksonchakma260@gmail.com>
2025-12-19 04:34:34 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,16 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDOmmWbMK2aOHUAQUqVRhooxf0XeEAYdow",
+  authDomain: "localchefbazaar-7262f.firebaseapp.com",
+  projectId: "localchefbazaar-7262f",
+  storageBucket: "localchefbazaar-7262f.firebasestorage.app",
+  messagingSenderId: "443949834162",
+  appId: "1:443949834162:web:800aa8c3cc17439d480eab"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
